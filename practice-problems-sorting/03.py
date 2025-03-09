905ad84924e936529e279f48a3581e14c60eab97
Jason Tsang <62657905+tsangsiu@users.noreply.github.com>
2025-03-09 22:46:31 +0800

    Complete practice problems on sorting

---
+++ practice-problems-sorting/03.py
@@ -0,0 +1,7 @@
+lst = [10, 9, -6, 11, 7, -16, 50, 8]
+lst.sort(key=str)
+print(lst)
+
+lst = [10, 9, -6, 11, 7, -16, 50, 8]
+lst.sort(key=str, reverse=True)
+print(lst)
---
+++ practice-problems-sorting/04.py
@@ -0,0 +1,23 @@
+books = [
+    {
+        'title': 'One Hundred Years of Solitude',
+        'author': 'Gabriel Garcia Marquez',
+        'published': '1967',
+    },
+    {
+        'title': 'The Book of Kells',
+        'author': 'Multiple Authors',
+        'published': '800',
+    },
+    {
+        'title': 'War and Peace',
+        'author': 'Leo Tolstoy',
+        'published': '1869',
+    },
+]
+
+def year_of_publication(book):
+    return int(book['published'])
+
+books.sort(key=year_of_publication)
+print(books)
